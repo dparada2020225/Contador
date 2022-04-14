c482db71fcc84aae28a536ed9a486fefb582c5d5
Denil José Parada Cabrera <dparada-2020225@kinal.edu.gt>
2022-04-13 19:25:58 -0600

    centrar la ventana

--- src/index.py
+++ src/index.py
@@ -2,23 +2,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# rutas simples
-@app.route('/test')
-def test():
-    return "Home Page 1 "
-
-@app.route('/test/about/')
-def about_test():
-    return "About Page 1"
-
 # Rutas
 @app.route('/')
 def home():
     return render_template("home.html")
 
-@app.route('/about', strict_slashes=False)
-def about():
-    return render_template("about.html")
 
 # para que se mantenga corriendo
 if __name__ == '__main__':
